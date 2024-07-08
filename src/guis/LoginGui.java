c138b1003df3045c7850a6594de5918b87459506
Hafsa-jabeen <hafsajabeen30@gmail.com>
2024-07-08 23:56:58 +0200

    final project

---
+++ src/guis/LoginGui.java
@@ -0,0 +1,87 @@
+package guis;
+
+import db_objs.MyJDBC;
+import db_objs.User;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+public class LoginGui extends BaseFrame{
+    public LoginGui(){
+        super("Banking App Login");
+    }
+
+    @Override
+    protected void addGuiComponents(){
+
+        JLabel bankingAppLabel = new JLabel("Banking Application");
+        bankingAppLabel.setBounds(0, 20, super.getWidth(), 40);
+        bankingAppLabel.setFont(new Font("Dialog", Font.BOLD, 32));
+        bankingAppLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(bankingAppLabel);
+
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameLabel.setBounds(20, 120, getWidth() - 30, 24);
+        usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(usernameLabel);
+
+        JTextField usernameField = new JTextField();
+        usernameField.setBounds(20, 160, getWidth() - 50, 40);
+        usernameField.setFont(new Font("Dialog", Font.PLAIN, 28));
+        add(usernameField);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(20, 280, getWidth() - 50, 24);
+        passwordLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(passwordLabel);
+
+
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(20, 320, getWidth() - 50, 40);
+        passwordField.setFont(new Font("Dialog", Font.PLAIN, 28));
+        add(passwordField);
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(20, 460, getWidth() - 50, 40);
+        loginButton.setFont(new Font("Dialog", Font.BOLD, 20));
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+
+                String password = String.valueOf(passwordField.getPassword());
+
+                User user = MyJDBC.validateLogin(username, password);
+
+                if(user != null){
+                    LoginGui.this.dispose();
+                    BankingAppGui bankingAppGui = new BankingAppGui(user);
+                    bankingAppGui.setVisible(true);
+                    JOptionPane.showMessageDialog(bankingAppGui, "Login Successfully!");
+                }else{
+                    JOptionPane.showMessageDialog(LoginGui.this, "Login failed...");
+                }
+            }
+        });
+        add(loginButton);
+
+        JLabel registerLabel = new JLabel("<html><a href=\"#\">Don't have an account? Register Here</a></html>");
+        registerLabel.setBounds(0, 510, getWidth() - 10, 30);
+        registerLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        registerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        registerLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+                LoginGui.this.dispose();
+                new RegisterGui().setVisible(true);
+            }
+        });
+
+        add(registerLabel);
+    }
+}
---
+++ src/guis/RegisterGui.java
@@ -0,0 +1,143 @@
+package guis;
+
+import db_objs.MyJDBC;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class RegisterGui extends BaseFrame{
+
+    public RegisterGui() {
+        super("Banking App Registration");
+    }
+    @Override
+    protected void addGuiComponents() {
+
+        JLabel bankingAppLabel = new JLabel("Banking Application");
+
+
+        bankingAppLabel.setBounds(0, 20, super.getWidth(), 40);
+
+
+        bankingAppLabel.setFont(new Font("Dialog", Font.BOLD, 32));
+
+
+        bankingAppLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+
+        add(bankingAppLabel);
+
+
+        JLabel usernameLabel = new JLabel("Username:");
+
+
+        usernameLabel.setBounds(20, 120, getWidth() - 30, 24);
+
+        usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(usernameLabel);
+
+
+        JTextField usernameField = new JTextField();
+        usernameField.setBounds(20, 160, getWidth() - 50, 40);
+        usernameField.setFont(new Font("Dialog", Font.PLAIN, 28));
+        add(usernameField);
+
+
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setBounds(20, 220, getWidth() - 50, 24);
+        passwordLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(passwordLabel);
+
+
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(20, 260, getWidth() - 50, 40);
+        passwordField.setFont(new Font("Dialog", Font.PLAIN, 28));
+        add(passwordField);
+
+
+        JLabel rePasswordLabel = new JLabel("Re-type Password:");
+        rePasswordLabel.setBounds(20, 320, getWidth() - 50, 40);
+        rePasswordLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        add(rePasswordLabel);
+
+
+        JPasswordField rePasswordField = new JPasswordField();
+        rePasswordField.setBounds(20, 360, getWidth() - 50, 40);
+        rePasswordField.setFont(new Font("Dialog", Font.PLAIN, 28));
+        add(rePasswordField);
+
+
+        JButton registerButton = new JButton("Register");
+        registerButton.setBounds(20, 460, getWidth() - 50, 40);
+        registerButton.setFont(new Font("Dialog", Font.BOLD, 20));
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String username = usernameField.getText();
+
+
+                String password = String.valueOf(passwordField.getPassword());
+
+
+                String rePassword = String.valueOf(rePasswordField.getPassword());
+
+                if(validateUserInput(username, password, rePassword)){
+
+                    if(MyJDBC.register(username, password)){
+
+                        RegisterGui.this.dispose();
+
+
+                        LoginGui loginGui = new LoginGui();
+                        loginGui.setVisible(true);
+
+
+                        JOptionPane.showMessageDialog(loginGui, "Registered Account Successfully!");
+                    }else{
+
+                        JOptionPane.showMessageDialog(RegisterGui.this, "Error: Username already taken");
+                    }
+                }else{
+
+                    JOptionPane.showMessageDialog(RegisterGui.this,
+                            "Error: Username must be at least 6 characters\n" +
+                                    "and/or Password must match");
+                }
+            }
+        });
+        add(registerButton);
+
+
+        JLabel loginLabel = new JLabel("<html><a href=\"#\">Have an account? Sign-in here</a></html>");
+        loginLabel.setBounds(0, 510, getWidth() - 10, 30);
+        loginLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
+        loginLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        loginLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+                RegisterGui.this.dispose();
+
+
+                new LoginGui().setVisible(true);
+            }
+        });
+        add(loginLabel);
+    }
+
+    private boolean validateUserInput(String username, String password, String rePassword){
+
+        if(username.length() == 0 || password.length() == 0 || rePassword.length() == 0) return false;
+
+        if(username.length() < 6) return false;
+
+        if(!password.equals(rePassword)) return false;
+
+        return true;
+    }
+}
